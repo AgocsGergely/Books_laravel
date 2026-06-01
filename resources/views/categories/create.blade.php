7da4ed6d582455030cb8483e3229d482c739373b
AgocsGergely <gergelybotond08@gmail.com>
2026-06-01 14:04:29 +0200

    Views folder + Routing

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,1 @@
+<h1>Új kategória</h1>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('categories', \App\Http\Controllers\CategoriesController::class);
+Route::resource('authors', \App\Http\Controllers\AuthorsController::class);
+Route::resource('publishers', \App\Http\Controllers\PublishersController::class);
+Route::resource('series', \App\Http\Controllers\SeriesController::class);
+Route::resource('books', \App\Http\Controllers\BooksController::class);
